9b43af4a8e59b971a70d72a088c1660675a4a12a
piotrek211 <piotr.rochala@ulb.be>
2021-07-08 22:24:46 +0200

    sdq

---
+++ experiments-loop-functions_cluster/build/loop-functions/moca/CMakeFiles/tutti_ag_loopfunc_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutti_ag_loopfunc_autogen"
+  "tutti_ag_loopfunc_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tutti_ag_loopfunc_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
